16d53e33220235bd35703c2b33c57e57cfe598c8
Vasiliy Mironov <90208494+razdva0@users.noreply.github.com>
2022-11-01 23:40:36 +0300

    Алгоритм Кнута - алгоритм поиска подстроки в строке

---
+++ knuth_morris_pratt.py
@@ -0,0 +1,30 @@
+def knuth_morris_pratt(haystack, needle):
+    needle = list(needle)
+
+    shifts = [1] * (len(needle) + 1)
+    shift = 1
+    for pos in range(len(needle)):
+        while shift <= pos and needle[pos] != needle[pos - shift]:
+            shift += shifts[pos - shift]
+        shifts[pos + 1] = shift
+
+    start_pos = 0
+    match_len = 0
+    for c in haystack:
+        while match_len == len(needle) or match_len >= 0 and needle[match_len] != c:
+            start_pos += shifts[match_len]
+            match_len -= shifts[match_len]
+        match_len += 1
+        if match_len == len(needle):
+            yield start_pos
+
+
+def main():
+    haystack = 'Hello, world! Hello, Python!'
+    needle = 'ello'
+    for x in knuth_morris_pratt(haystack, needle):
+        print(x)
+
+
+if __name__ == '__main__':
+    main()
